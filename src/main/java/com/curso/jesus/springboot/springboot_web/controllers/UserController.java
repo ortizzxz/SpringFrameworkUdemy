4bc6cc0cf8c838b67d2344f6ec4a19d5581a74af
jesusO <jesusdortiz18@gmail.com>
2024-06-03 15:14:42 +0200

    Mi primer hola mundo Controlador estatico

--- src/main/java/com/curso/jesus/springboot/springboot_web/controllers/UserController.java
+++ src/main/java/com/curso/jesus/springboot/springboot_web/controllers/UserController.java
@@ -1,15 +1,17 @@
 package com.curso.jesus.springboot.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     //El controlador maneja el Request y devuelve un Response al usuario
 
     // Un metodo handle maneja una peticion de Request
-    
+    @GetMapping("/details")
     public String details(){
 
         return "details"; 
     }
+    
 }
