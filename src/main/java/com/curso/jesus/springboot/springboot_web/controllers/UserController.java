a98ec1afd1b09dc7c6186387ab7b82289375afbd
jesusO <jesusdortiz18@gmail.com>
2024-06-04 12:07:02 +0200

    IF Thymeleaf - Controlador y Logica

--- src/main/java/com/curso/jesus/springboot/springboot_web/controllers/UserController.java
+++ src/main/java/com/curso/jesus/springboot/springboot_web/controllers/UserController.java
@@ -1,9 +1,11 @@
 package com.curso.jesus.springboot.springboot_web.controllers;
 
+import java.util.ArrayList;
 import java.util.Map;
-
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.curso.jesus.springboot.springboot_web.models.User;
@@ -19,10 +21,21 @@ public class UserController {
         User user = new User("Jesus", "Ortiz");
         model.addAttribute("title", "Bienvenido Usuario");
         model.addAttribute("user", user);
+        user.setEmail("jesus@jesus.com");
+
         return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Listado de usuarios");
+        return "list";
+    }
     
-    //ESTO ES USANDO MAP
+    /*ESTO ES USANDO MAP
     public String details(Map<String, Object> model){ //Para usar Model lo "inyectamos" en el String con el metodo MODEL
 
         //un modelAddAttribute es como su nombre indica un declarador de atributo para reutilizar
@@ -30,7 +43,7 @@ public class UserController {
         model.put("name", "Jesus");
         model.put("lastname", "Ortiz");
         return "details"; 
-    }
+    }*/
 
     
 }
--- src/main/java/com/curso/jesus/springboot/springboot_web/models/User.java
+++ src/main/java/com/curso/jesus/springboot/springboot_web/models/User.java
@@ -3,6 +3,7 @@ package com.curso.jesus.springboot.springboot_web.models;
 public class User {
     private String name;
     private String lastname;
+    private String email;
 
     public User(String name, String lastname) {
         this.name = name;
@@ -21,4 +22,13 @@ public class User {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 }
