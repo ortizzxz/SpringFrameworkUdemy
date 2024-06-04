a3bb52caa1f0213a708ed84e67178c0ff5a0e711
jesusO <jesusdortiz18@gmail.com>
2024-06-04 14:34:22 +0200

    trabajando con postman y metodos POST / @PostMapping

--- src/main/java/com/curso/jesus/springboot/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/curso/jesus/springboot/springboot_web/controllers/PathVariableController.java
@@ -2,6 +2,7 @@ package com.curso.jesus.springboot.springboot_web.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.curso.jesus.springboot.springboot_web.models.User;
 import com.curso.jesus.springboot.springboot_web.models.dto.ParamDto;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -37,6 +40,15 @@ public class PathVariableController {
         json.put("id", id);
 
         return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+           
+        //save user en la bbdd
+        
+        
+        return user;
     }
 }
 
