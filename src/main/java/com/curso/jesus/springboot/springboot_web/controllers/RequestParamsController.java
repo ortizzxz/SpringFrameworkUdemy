dd94494b6c38adde4cc898ebdb972f78e3959948
jesusO <jesusdortiz18@gmail.com>
2024-06-04 13:43:36 +0200

    manejo de excepciones al enviar parametros de tipo de dato invalido

--- src/main/java/com/curso/jesus/springboot/springboot_web/controllers/RequestParamsController.java
+++ src/main/java/com/curso/jesus/springboot/springboot_web/controllers/RequestParamsController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.jesus.springboot.springboot_web.models.dto.ParamDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,10 +31,26 @@ public class RequestParamsController {
         ParamDto params = new ParamDto();
         params.setMessage(text);
         params.setCode(code);
-        
+
         return params;
     }
     
+    @GetMapping("/request")
+    public ParamDto request(HttpServletRequest request){
+        Integer code = 0; 
+
+        try{
+            code = Integer.parseInt(request.getParameter("code"));
+        }catch(NumberFormatException e){
+        }
+
+        ParamDto params = new ParamDto();
+
+        // params.setCode(Integer.parseInt(request.getParameter("code")));
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
+        return params;
+    }
     /*Explicacion de este @RestController -> Basicamente lo que hago es que "foo" tenga un
      * Request param obligatorio que va a ser de tipo String en donde ese "message" va a ser 
      * el que se use para hacer el param.setMessage() -> es decir, el parametro de entrada
