44b875b289f5dedcd81d0ed4164ee63bc0e61da2
jesusO <jesusdortiz18@gmail.com>
2024-06-05 11:18:20 +0200

    forward y redirect

---
+++ src/main/java/com/curso/jesus/springboot/springboot_web/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.curso.jesus.springboot.springboot_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"","/","/home"})
+    public String home(){
+
+
+        return "redirect:/details";
+    }
+
+}
--- src/main/java/com/curso/jesus/springboot/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/curso/jesus/springboot/springboot_web/controllers/PathVariableController.java
@@ -1,21 +1,21 @@
 package com.curso.jesus.springboot.springboot_web.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.curso.jesus.springboot.springboot_web.models.User;
-import com.curso.jesus.springboot.springboot_web.models.dto.ParamDto;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.jesus.springboot.springboot_web.models.User;
+import com.curso.jesus.springboot.springboot_web.models.dto.ParamDto;
 
 
 
@@ -36,6 +36,12 @@ public class PathVariableController {
     @Value("${config.listOfValues}")
     private List<String> listOfValues;
 
+    @Value("#{${config.valuesMap}}")
+    private Map<String, Object> valuesMap;
+
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/values")
     public Map<String,Object> values(){
 
@@ -43,9 +49,11 @@ public class PathVariableController {
 
         json.put("code", code);
         json.put("username", username);
+        json.put("usernameEnv", environment.getProperty("config.username"));
         json.put("message", message);
         json.put("listOfValues", listOfValues);
-
+        json.put("valuesMap", valuesMap);
+        
         return json;
     }
 
