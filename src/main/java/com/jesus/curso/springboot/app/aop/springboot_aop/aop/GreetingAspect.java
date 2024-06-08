b57d7bf31fa34e855ad3ea3fdce1ad155a76de90
jesusO <jesusdortiz18@gmail.com>
2024-06-08 11:25:45 +0200

    loggerAfter

--- src/main/java/com/jesus/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/jesus/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -2,6 +2,7 @@ package com.jesus.curso.springboot.app.aop.springboot_aop.aop;
 
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -15,11 +16,22 @@ import java.util.Arrays;
 public class GreetingAspect {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    //@Before("execution(String com.jesus.curso.springboot.app.aop.springboot_aop...*.*(..))")   
+    //@Before("execution(String com.jesus.curso.springboot.app.aop.springboot_aop.services.*.*(..))")   
+    //@Before("execution(String com.jesus.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")   
     @Before("execution(String com.jesus.curso.springboot.app.aop.springboot_aop.services.GreetingService.sayHello(..))") // (..) -> expresion regular
     public void loggerBefore(JoinPoint joinPoint){
 
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Antes: " + method + ", con los argumentos : " + args);
+    }
+ 
+    @After("execution(String com.jesus.curso.springboot.app.aop.springboot_aop.services.GreetingService.sayHello(..))")
+    public void loggerAfter(JoinPoint joinPoint){
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Después: " + method + ", con los argumentos : " + args);
     }
 }
