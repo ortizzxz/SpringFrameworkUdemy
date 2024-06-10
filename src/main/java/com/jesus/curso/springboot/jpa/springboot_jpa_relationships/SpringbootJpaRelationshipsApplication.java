1e5828da2a1b9a080aaed9480155e40d2574a597
jesusO <jesusdortiz18@gmail.com>
2024-06-10 17:20:53 +0200

    adding local repo

---
+++ src/main/java/com/jesus/curso/springboot/jpa/springboot_jpa_relationships/SpringbootJpaRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.jesus.curso.springboot.jpa.springboot_jpa_relationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaRelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaRelationshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesus/curso/springboot/jpa/springboot_jpa_relationships/SpringbootJpaRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.curso.springboot.jpa.springboot_jpa_relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
