365da04062c3ddac45cdb5956db14449478d6f3d
jesusO <jesusdortiz18@gmail.com>
2024-06-10 17:32:42 +0200

    creacion de clases y @ManyToOne

---
+++ src/main/java/com/jesus/curso/springboot/jpa/springboot_jpa_relationships/entities/Invoice.java
@@ -0,0 +1,56 @@
+package com.jesus.curso.springboot.jpa.springboot_jpa_relationships.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="invoices")
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // -> AutoIncrement
+    private Long id;
+
+    private String description;
+    private Long total;
+
+    @ManyToOne // -> many invoices linked to one client
+    private Client client;
+    
+    public Invoice(){}
+
+    public Invoice(String description, Long total) {
+        this.description = description;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Long getTotal() {
+        return total;
+    }
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "{id=" + id + ", description=" + description + ", total=" + total + ", client=" + client + "}";
+    }
+
+    
+}
