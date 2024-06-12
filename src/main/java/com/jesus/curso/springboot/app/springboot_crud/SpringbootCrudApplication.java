3a5f9c73f99684df2e08c1c727eae031abd0a2c2
jesusO <jesusdortiz18@gmail.com>
2024-06-12 15:42:50 +0200

    personalizando errores

--- src/main/java/com/jesus/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
+++ src/main/java/com/jesus/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
@@ -2,8 +2,10 @@ package com.jesus.curso.springboot.app.springboot_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:message.properties")
 public class SpringbootCrudApplication {
 
 	public static void main(String[] args) {
