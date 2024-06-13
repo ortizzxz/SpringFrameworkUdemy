442846014cdcbf450f255aaec4081747e21e7208
jesusO <jesusdortiz18@gmail.com>
2024-06-13 11:19:33 +0200

    fixing issue BucleInfinito y NoShowingRoles

--- springboot-crud/src/main/java/com/jesus/curso/springboot/app/springboot_crud/entities/User.java
+++ springboot-crud/src/main/java/com/jesus/curso/springboot/app/springboot_crud/entities/User.java
@@ -1,9 +1,11 @@
 package com.jesus.curso.springboot.app.springboot_crud.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.annotations.ManyToAny;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -44,6 +46,7 @@ public class User {
         enabled = true;
     }
 
+    @JsonIgnoreProperties({"users", "handler", "hibernateLazyInitializer"})
     @ManyToAny
     @JoinTable(
         name = "user_roles",
@@ -56,6 +59,9 @@ public class User {
     @Transient
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // -> no se accede al JSON cuando se hace un get
     private boolean admin;
+
+
+    public User(){ roles = new ArrayList<>();}
 
     public Long getId() {
         return id;
@@ -103,6 +109,37 @@ public class User {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
     }
 
     
