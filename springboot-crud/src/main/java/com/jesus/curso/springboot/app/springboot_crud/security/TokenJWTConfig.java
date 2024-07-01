68ef7fcba8b23a2a509c6d1bc2221bbabfec2ebd
jesusO <jesusdortiz18@gmail.com>
2024-07-01 13:29:42 +0200

    filter

--- springboot-crud/src/main/java/com/jesus/curso/springboot/app/springboot_crud/security/TokenJWTConfig.java
+++ springboot-crud/src/main/java/com/jesus/curso/springboot/app/springboot_crud/security/TokenJWTConfig.java
@@ -5,6 +5,7 @@ import javax.crypto.SecretKey;
 import io.jsonwebtoken.Jwts;
 
 public class TokenJWTConfig {
+    
     public static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build(); // -> clave secreta
     public static final String PREFIX_TOKEN = "Bearer ";
     public static final String HEADER_AUTHORIZATION = "Authorization";
